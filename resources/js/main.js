53583a73e879ebab82795629bde81ceace61b97b
DocSkill <larbisamuel0@gmail.com>
2017-10-19 21:08:15 +0100

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,140 @@
+$(function () {
+	
+	// var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')): {
+	// 	todo: [],
+	// 	completed: []
+	// };
+
+	
+
+	
+	// Remove SVG goes here 
+	var removeSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 32"><g data-name="Layer 2"><g data-name="Layer 1"><path class="fill" d="M29,5H21V1a.94.94,0,0,0-1-1H10A.94.94,0,0,0,9,1V5H1A.94.94,0,0,0,0,6,.94.94,0,0,0,1,7H3V31a.94.94,0,0,0,1,1H26a.94.94,0,0,0,1-1V7h2a.94.94,0,0,0,1-1A.94.94,0,0,0,29,5ZM11,25a.94.94,0,0,1-1,1,.94.94,0,0,1-1-1V14a.94.94,0,0,1,1-1,.94.94,0,0,1,1,1ZM11,2h8V5H11Zm5,23a1,1,0,0,1-2,0V14a1,1,0,0,1,2,0Zm5,0a1,1,0,0,1-2,0V14a1,1,0,0,1,2,0Z"/></g></g></svg>';
+
+	// renderTodoList();
+
+	$('#addBtn').on('click', function() {
+		var value = $('#item').val();
+		if (value) {
+
+			addItemTodo(value);
+			$('#item').val("");
+
+
+
+		}
+
+		if (value == "") {
+			$('#item').addClass('animated shake').one('webkitanimationEnd mozanimationEnd MSanimationEnd onanimationend animationend',
+				function() {
+					$(this).removeClass('animated shake');
+				});
+		}
+
+		// data.todo.push(value);
+		// dataObjectUpdated();
+		
+	});
+
+	$('#item').on('keydown', function (e) {
+		var value = this.value
+		if (e.code === 'Enter' && value ) {
+			addItemTodo(value);
+		}
+	});
+
+	// function renderTodoList() {
+	// 	if (!data.todo.length && !data.completed.length) return;
+
+	// 	for (var i = 0; i < data.todo.length; i++) {
+	// 		var value = data.todo[i];
+	// 		addItemTodo(value);
+	// 	}
+
+	// 	for (var j = 0; j < data.completed.length; j++) {
+	// 		var value = data.completed[j];
+	// 		addItemTodo(value,true);
+	// 	}
+	// }
+
+	// function dataObjectUpdated() {
+	// 	localStorage.setItem('todoList', JSON.stringify(data));
+	// }
+
+	// Remove item
+	function removeItem() {
+		var item = this;
+		var parent = item.parentNode;
+		// var id = parent.Id;
+		// var value = item.innerText;
+
+		// if (id ==='todo') {
+		// 	data.todo.splice(data.todo.indexOf(value), 1);
+		// } else {
+		// 	data.completed.splice(data.completed.indexOf(value), 1);
+		// }
+
+		// dataObjectUpdated();
+
+		parent.remove(item);
+
+		
+
+	}
+
+	function completeItem() {
+		$(this).toggleClass('completedTaskCircle');
+		$(this.parentNode).toggleClass('completedTaskList');
+		// var parent = this.parentNode;
+		// var id = parent.id;
+		// var value = parent.innerText;
+
+		// if (id ==='todo') {
+		// 	data.todo.splice(data.todo.indexOf(value), 1);
+		// 	data.completed.push(value);
+		// } else {
+		// 	data.completed.splice(data.completed.indexOf(value), 1);
+		// 	data.completed.push(value);
+		// }
+
+		// dataObjectUpdated();
+
+
+	}
+
+
+	// Adds new item to the list
+	function addItemTodo(text, completed) {
+		var list = (completed) ?$('#completed'):$('#todo');
+
+		var item = document.createElement('li');
+		item.innerText = text; 
+
+		var circle = document.createElement('div');
+		circle.classList.add('circle');
+
+		$(circle).on('click', completeItem);
+
+		var remove = document.createElement('div');
+		remove.classList.add('trashCan');
+		remove.innerHTML = removeSVG;
+
+		$(remove).on('click', removeItem);
+
+		item.append(circle);
+		item.append(remove);
+
+		list.prepend(item);
+
+		$('li').addClass('animated slideInDown').one('webkitanimationEnd mozanimationEnd MSanimationEnd onanimationend animationend',
+			function() {
+				$(this).removeClass('animated slideInDown');
+			});
+
+		// dataObjectUpdated();
+	
+	}
+
+
+});
+
